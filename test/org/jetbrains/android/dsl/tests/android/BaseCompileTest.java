ee7816867209daf734f693bb5d81b64d4c70cf05
Yan Zhulanow <yan.zhulanow@jetbrains.org>
2014-10-16 16:36:47 +0400

    Fix compile tests.

--- test/org/jetbrains/android/dsl/tests/android/BaseCompileTest.java
+++ test/org/jetbrains/android/dsl/tests/android/BaseCompileTest.java
@@ -19,7 +19,6 @@ package org.jetbrains.android.dsl.tests.android;
 import org.jetbrains.android.dsl.DSLGenerator;
 import org.jetbrains.android.dsl.utils.DirectoryFilter;
 import org.jetbrains.android.dsl.utils.JarFilter;
-import org.jetbrains.android.dsl.tests.TestGeneratorProps;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -31,7 +30,7 @@ import java.util.List;
 
 public class BaseCompileTest extends Assert {
     private final String kotlincFilename = "lib/kotlinc/bin/kotlinc-jvm";
-    final String tmpJarFile = this.getClass() + "out.jar";
+    final String tmpJarFile = /*this.getClass() + */ "out.jar";
 
     private static final FileFilter directoryFilter = new DirectoryFilter();
     private static final FileFilter jarFilter = new JarFilter();
@@ -58,52 +57,69 @@ public class BaseCompileTest extends Assert {
 
         File[] versions = new File("original/").listFiles(directoryFilter);
         for (File ver: versions) {
-            String fVersion = ver.getName();
-            int version = Integer.parseInt(fVersion.replaceAll("[^0-9]", ""));
-
-            List<File> jarFiles = Arrays.asList(ver.listFiles(jarFilter));
-            List<String> jarFilesString = new ArrayList<>();
-            StringBuilder classpath = new StringBuilder();
-            for (File f: jarFiles) {
-                jarFilesString.add(f.getAbsolutePath());
-                if (classpath.length()>0) {
-                    classpath.append(File.pathSeparatorChar);
-                }
-                classpath.append(f.getPath());
-            }
-            TestGeneratorProps settings = new AndroidTestGeneratorProps();
-            DSLGenerator gen = new DSLGenerator(version, fVersion, jarFilesString, settings);
-            gen.run();
-            String kotlincArgs[] = {
-                kotlincFilename,
-                "-jar", tmpJarFile,
-                "-classpath", classpath.toString(),
-                testData.getPath()
-            };
-            ArrayList<String> args = new ArrayList<>(Arrays.asList(kotlincArgs));
-            for (File file : settings.tmpFiles.values()) {
-                args.add(file.getAbsolutePath());
-            }
-            ProcResult res = compile(args.toArray(new String[args.size()]));
-            assertEquals(res.stderr, "");
-            for (File file : settings.tmpFiles.values()) {
+            AndroidTestGeneratorProps props = compile(testData, ver);
+            for (File file : props.tmpFiles.values()) {
                 file.delete();
             }
         }
     }
 
-    protected ProcResult compile(String[] args) throws IOException, InterruptedException {
-        Process p = Runtime.getRuntime().exec(args);
-        BufferedReader br1 = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        BufferedReader br2 = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-        StringBuilder errors = new StringBuilder();
-        String str;
-        while (((str = br1.readLine()) != null) && (br2.readLine() != null)) {
-            if (str.startsWith("ERROR"))
-                errors.append(str);
+    private AndroidTestGeneratorProps compile(File testData, File ver) throws IOException, InterruptedException {
+        String fVersion = ver.getName();
+        int version = Integer.parseInt(fVersion.replaceAll("[^0-9]", ""));
+
+        List<File> jarFiles = Arrays.asList(ver.listFiles(jarFilter));
+        List<String> jarFilesString = new ArrayList<>();
+        StringBuilder classpath = new StringBuilder();
+        for (File f: jarFiles) {
+            jarFilesString.add(f.getAbsolutePath());
+            if (classpath.length()>0) {
+                classpath.append(File.pathSeparatorChar);
+            }
+            classpath.append(f.getPath());
         }
+        AndroidTestGeneratorProps props = new AndroidTestGeneratorProps();
+        DSLGenerator gen = new DSLGenerator(version, fVersion, jarFilesString, props);
+        gen.run();
+        String kotlincArgs[] = {
+          kotlincFilename,
+          "-d", tmpJarFile,
+          "-classpath", classpath.toString(),
+          testData.getPath()
+        };
+        ArrayList<String> args = new ArrayList<>(Arrays.asList(kotlincArgs));
+        for (File file : props.tmpFiles.values()) {
+            args.add(file.getAbsolutePath());
+        }
+        ProcResult res = runCompiler(args.toArray(new String[args.size()]));
+        assertEquals(res.stderr, "");
+        assertEquals(res.exitCode, 0);
+        return props;
+    }
+
+    protected ProcResult runCompiler(String[] args) throws IOException, InterruptedException {
+        Process p = Runtime.getRuntime().exec(args);
+        BufferedReader brInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        BufferedReader brError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+        StringBuilder errors = new StringBuilder();
+        StringBuilder output = new StringBuilder();
+        String str;
+
+        while ((str = brInput.readLine()) != null) {
+            if (str.startsWith("ERROR"))
+                errors.append(str).append("\n");
+            else
+                output.append(str).append("\n");
+        }
+        while ((str = brError.readLine()) != null) {
+            if (str.startsWith("ERROR"))
+                errors.append(str).append("\n");
+            else
+                output.append(str).append("\n");
+        }
+
         p.waitFor();
-        return new ProcResult(errors.toString(), p.exitValue());
+        return new ProcResult(output.toString(), errors.toString(), p.exitValue());
     }
 
     @AfterMethod
@@ -113,10 +129,12 @@ public class BaseCompileTest extends Assert {
 
     class ProcResult {
         public String stderr;
+        public String stdout;
         public int exitCode;
 
-        ProcResult(String stderr, int exitCode) {
+        ProcResult(String stdout, String stderr, int exitCode) {
             this.stderr = stderr;
+            this.stdout = stdout;
             this.exitCode = exitCode;
         }
     }
