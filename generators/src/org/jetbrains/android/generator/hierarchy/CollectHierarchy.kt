c83e8c3253055b02c4b176de06d796cde01e68e9
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-02-13 17:40:48 +0300

    Implement View hierarchy collector (for xml->dsl converter)

---
+++ generators/src/org/jetbrains/android/generator/hierarchy/CollectHierarchy.kt
@@ -0,0 +1,55 @@
+package org.jetbrains.android.generator.hierarchy
+
+import java.io.File
+import org.jetbrains.android.dsl.ClassProcessor
+import org.jetbrains.android.dsl.isInner
+import org.jetbrains.android.dsl.isView
+import org.objectweb.asm.tree.ClassNode
+import org.jetbrains.android.dsl.ClassTree
+import org.jetbrains.android.dsl.ClassTreeNode
+import kotlin.platform.platformStatic
+import com.google.gson.Gson
+
+public fun main(args: Array<String>): Unit = HierarchyCollector.collect()
+
+object HierarchyCollector {
+
+    platformStatic
+    public fun collect() {
+        val ver = File("original").listFiles { it.name.matches("[0-9]+") }!!
+                .first { it.listFiles { it.name == "android.jar" }?.isNotEmpty() ?: false }
+        val androidJar = ver.listFiles { it.name == "android.jar" }!!.first().getAbsolutePath()
+
+        val classTree = ClassProcessor(listOf(androidJar)).genClassTree()
+        val viewClasses = classTree.filter { it.isView(classTree) && !it.isInner() && it.name.startsWith("android/widget/") }
+
+        val hierarchy = viewClasses.map {
+            it.name.prettify() to getSuperViews(it, classTree)
+        }.filter { it.second.isNotEmpty() }.toMap()
+
+        File("views.json").writeText(Gson().toJson(hierarchy))
+    }
+
+    private fun getSuperViews(node: ClassNode, tree: ClassTree): List<String> {
+        val list = arrayListOf<String>()
+
+        fun getSuperViews(node: ClassTreeNode) {
+            val parent = node.parent
+            if (parent != null && parent.data.isView(tree) && parent.data.name != "android/view/View") {
+                list.add(parent.data.name.prettify())
+                getSuperViews(parent)
+            }
+        }
+
+        val treeNode = tree.findNode(node)
+
+        if (treeNode != null) {
+            getSuperViews(treeNode)
+        }
+
+        return list
+    }
+
+    private fun String.prettify(): String = replace("android/view/", "").replace("android/widget/", "")
+
+}
