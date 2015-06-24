017494b7e7b4c4a72af44a7ba309e5162566eeb0
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-06-24 13:44:51 +0300

    Support Throwable argument in Logger

--- dsl/static/src/Logger.kt
+++ dsl/static/src/Logger.kt
@@ -31,33 +31,61 @@ public interface AnkoLogger {
             }
         }
 
-    protected final fun verbose(message: Any?) {
+    protected final fun verbose(message: Any?, thr: Throwable? = null) {
         val tag = loggerTag
-        if (Log.isLoggable(tag, Log.VERBOSE)) Log.v(tag, message?.toString() ?: "null")
+        if (Log.isLoggable(tag, Log.VERBOSE)) {
+            if (thr != null)
+                Log.v(tag, message?.toString() ?: "null", thr)
+            else
+                Log.v(tag, message?.toString() ?: "null")
+        }
     }
 
-    protected final fun debug(message: Any?) {
+    protected final fun debug(message: Any?, thr: Throwable? = null) {
         val tag = loggerTag
-        if (Log.isLoggable(tag, Log.DEBUG)) Log.d(tag, message?.toString() ?: "null")
+        if (Log.isLoggable(tag, Log.DEBUG)) {
+            if (thr != null)
+                Log.d(tag, message?.toString() ?: "null", thr)
+            else
+                Log.d(tag, message?.toString() ?: "null")
+        }
     }
 
-    protected final fun info(message: Any?) {
+    protected final fun info(message: Any?, thr: Throwable? = null) {
         val tag = loggerTag
-        if (Log.isLoggable(tag, Log.INFO)) Log.i(tag, message?.toString() ?: "null")
+        if (Log.isLoggable(tag, Log.INFO)) {
+            if (thr != null)
+                Log.i(tag, message?.toString() ?: "null", thr)
+            else
+                Log.i(tag, message?.toString() ?: "null")
+        }
     }
 
-    protected final fun warn(message: Any?) {
+    protected final fun warn(message: Any?, thr: Throwable? = null) {
         val tag = loggerTag
-        if (Log.isLoggable(tag, Log.WARN)) Log.w(tag, message?.toString() ?: "null")
+        if (Log.isLoggable(tag, Log.WARN)) {
+            if (thr != null)
+                Log.w(tag, message?.toString() ?: "null", thr)
+            else
+                Log.w(tag, message?.toString() ?: "null")
+        }
     }
 
-    protected final fun error(message: Any?) {
+    protected final fun error(message: Any?, thr: Throwable? = null) {
         val tag = loggerTag
-        if (Log.isLoggable(tag, Log.ERROR)) Log.e(tag, message?.toString() ?: "null")
+        if (Log.isLoggable(tag, Log.ERROR)) {
+            if (thr != null)
+                Log.e(tag, message?.toString() ?: "null", thr)
+            else
+                Log.e(tag, message?.toString() ?: "null")
+        }
     }
 
-    protected final fun wtf(message: Any?) {
-        Log.v(loggerTag, message?.toString() ?: "null")
+    protected final fun wtf(message: Any?, thr: Throwable? = null) {
+        if (thr != null)
+            Log.wtf(loggerTag, message?.toString() ?: "null", thr)
+        else
+            Log.wtf(loggerTag, message?.toString() ?: "null")
     }
 
     /* lazy */
