7c69bf086edc0ec2f3eb6c51f3236a4354ef95d2
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-09-01 19:15:50 +0300

    Replace AnkoLogger functions with the extension functions

--- dsl/static/src/common/Logger.kt
+++ dsl/static/src/common/Logger.kt
@@ -28,95 +28,100 @@ public interface AnkoLogger {
                 tag.substring(0, 23)
             }
         }
+}
 
-    protected final fun verbose(message: Any?, thr: Throwable? = null) {
-        val tag = loggerTag
-        if (Log.isLoggable(tag, Log.VERBOSE)) {
-            if (thr != null)
-                Log.v(tag, message?.toString() ?: "null", thr)
-            else
-                Log.v(tag, message?.toString() ?: "null")
+public fun AnkoLogger.verbose(message: Any?, thr: Throwable? = null) {
+    log(this, message, thr, Log.VERBOSE,
+            { tag, msg -> Log.v(tag, msg) },
+            { tag, msg, thr -> Log.v(tag, msg, thr) })
+}
+
+public fun AnkoLogger.debug(message: Any?, thr: Throwable? = null) {
+    log(this, message, thr, Log.DEBUG,
+            { tag, msg -> Log.d(tag, msg) },
+            { tag, msg, thr -> Log.d(tag, msg, thr) })
+}
+
+public fun AnkoLogger.info(message: Any?, thr: Throwable? = null) {
+    log(this, message, thr, Log.INFO,
+            { tag, msg -> Log.i(tag, msg) },
+            { tag, msg, thr -> Log.i(tag, msg, thr) })
+}
+
+public fun AnkoLogger.warn(message: Any?, thr: Throwable? = null) {
+    log(this, message, thr, Log.WARN,
+            { tag, msg -> Log.w(tag, msg) },
+            { tag, msg, thr -> Log.w(tag, msg, thr) })
+}
+
+public fun AnkoLogger.error(message: Any?, thr: Throwable? = null) {
+    log(this, message, thr, Log.ERROR,
+            { tag, msg -> Log.e(tag, msg) },
+            { tag, msg, thr -> Log.e(tag, msg, thr) })
+}
+
+private inline fun log(
+        logger: AnkoLogger,
+        message: Any?,
+        thr: Throwable?,
+        level: Int,
+        f: (String, String) -> Unit,
+        fThrowable: (String, String, Throwable) -> Unit) {
+    val tag = logger.loggerTag
+    if (Log.isLoggable(tag, level)) {
+        if (thr != null) {
+            fThrowable(tag, message?.toString() ?: "null", thr)
+        } else {
+            f(tag, message?.toString() ?: "null")
         }
     }
+}
 
-    protected final fun debug(message: Any?, thr: Throwable? = null) {
-        val tag = loggerTag
-        if (Log.isLoggable(tag, Log.DEBUG)) {
-            if (thr != null)
-                Log.d(tag, message?.toString() ?: "null", thr)
-            else
-                Log.d(tag, message?.toString() ?: "null")
-        }
+public fun AnkoLogger.wtf(message: Any?, thr: Throwable? = null) {
+    if (thr != null) {
+        Log.wtf(loggerTag, message?.toString() ?: "null", thr)
+    } else {
+        Log.wtf(loggerTag, message?.toString() ?: "null")
     }
+}
 
-    protected final fun info(message: Any?, thr: Throwable? = null) {
-        val tag = loggerTag
-        if (Log.isLoggable(tag, Log.INFO)) {
-            if (thr != null)
-                Log.i(tag, message?.toString() ?: "null", thr)
-            else
-                Log.i(tag, message?.toString() ?: "null")
-        }
+public inline fun AnkoLogger.verbose(message: () -> Any?) {
+    val tag = loggerTag
+    if (Log.isLoggable(tag, Log.VERBOSE)) {
+        Log.v(tag, message()?.toString() ?: "null")
     }
+}
 
-    protected final fun warn(message: Any?, thr: Throwable? = null) {
-        val tag = loggerTag
-        if (Log.isLoggable(tag, Log.WARN)) {
-            if (thr != null)
-                Log.w(tag, message?.toString() ?: "null", thr)
-            else
-                Log.w(tag, message?.toString() ?: "null")
-        }
+public inline fun AnkoLogger.debug(message: () -> Any?) {
+    val tag = loggerTag
+    if (Log.isLoggable(tag, Log.DEBUG)) {
+        Log.d(tag, message()?.toString() ?: "null")
     }
+}
 
-    protected final fun error(message: Any?, thr: Throwable? = null) {
-        val tag = loggerTag
-        if (Log.isLoggable(tag, Log.ERROR)) {
-            if (thr != null)
-                Log.e(tag, message?.toString() ?: "null", thr)
-            else
-                Log.e(tag, message?.toString() ?: "null")
-        }
+public inline fun AnkoLogger.info(message: () -> Any?) {
+    val tag = loggerTag
+    if (Log.isLoggable(tag, Log.INFO)) {
+        Log.i(tag, message()?.toString() ?: "null")
     }
+}
 
-    protected final fun wtf(message: Any?, thr: Throwable? = null) {
-        if (thr != null)
-            Log.wtf(loggerTag, message?.toString() ?: "null", thr)
-        else
-            Log.wtf(loggerTag, message?.toString() ?: "null")
+public inline fun AnkoLogger.warn(message: () -> Any?) {
+    val tag = loggerTag
+    if (Log.isLoggable(tag, Log.WARN)) {
+        Log.w(tag, message()?.toString() ?: "null")
     }
+}
 
-    /* lazy */
-
-    protected final inline fun verbose(message: () -> Any?) {
-        val tag = loggerTag
-        if (Log.isLoggable(tag, Log.VERBOSE)) Log.v(tag, message()?.toString() ?: "null")
+public inline fun AnkoLogger.error(message: () -> Any?) {
+    val tag = loggerTag
+    if (Log.isLoggable(tag, Log.ERROR)) {
+        Log.e(tag, message()?.toString() ?: "null")
     }
+}
 
-    protected final inline fun debug(message: () -> Any?) {
-        val tag = loggerTag
-        if (Log.isLoggable(tag, Log.DEBUG)) Log.d(tag, message()?.toString() ?: "null")
-    }
-
-    protected final inline fun info(message: () -> Any?) {
-        val tag = loggerTag
-        if (Log.isLoggable(tag, Log.INFO)) Log.i(tag, message()?.toString() ?: "null")
-    }
-
-    protected final inline fun warn(message: () -> Any?) {
-        val tag = loggerTag
-        if (Log.isLoggable(tag, Log.WARN)) Log.w(tag, message()?.toString() ?: "null")
-    }
-
-    protected final inline fun error(message: () -> Any?) {
-        val tag = loggerTag
-        if (Log.isLoggable(tag, Log.ERROR)) Log.e(tag, message()?.toString() ?: "null")
-    }
-
-    protected final inline fun wtf(message: () -> Any?) {
-        Log.v(loggerTag, message()?.toString() ?: "null")
-    }
-
+public inline fun AnkoLogger.wtf(message: () -> Any?) {
+    Log.v(loggerTag, message()?.toString() ?: "null")
 }
 
 public fun Throwable.getStackTraceString(): String = Log.getStackTraceString(this)
