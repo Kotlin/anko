df67769c185af8414efc1358d5bfc1e7da06443e
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-09-01 19:03:56 +0300

    Add functions that iterate collections and arrays without using an iterator

---
+++ dsl/static/src/common/Arrays.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.anko.collections
+
+inline fun <T> Array<T>.forEachByIndex(f: (T) -> Unit) {
+    val lastIndex = size() - 1
+    for (i in 0..lastIndex) {
+        f(get(i))
+    }
+}
+
+inline fun <T> Array<T>.forEachWithIndex(f: (Int, T) -> Unit) {
+    val lastIndex = size() - 1
+    for (i in 0..lastIndex) {
+        f(i, get(i))
+    }
+}
+
+inline fun <T> Array<T>.forEachReversed(f: (T) -> Unit) {
+    var i = size() - 1
+    while (i >= 0) {
+        f(get(i))
+        i--
+    }
+}
+
+inline fun <T> Array<T>.forEachReversedWithIndex(f: (Int, T) -> Unit) {
+    var i = size() - 1
+    while (i >= 0) {
+        f(i, get(i))
+        i--
+    }
+}
