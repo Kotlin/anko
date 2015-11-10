e2e434cf2406eb04cd7e8221170c8d493b09b185
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-11-10 16:14:45 +0300

    Remove deprecated __dslAddView functions

--- dsl/static/src/common/Ui.kt
+++ dsl/static/src/common/Ui.kt
@@ -22,32 +22,11 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewManager
-import org.jetbrains.anko.custom.ankoView
 import org.jetbrains.anko.internals.AnkoInternals.NoBinding
 
 fun <T : View> T.style(style: (View) -> Unit): T {
     applyStyle(this, style)
     return this
-}
-
-@Deprecated("Use ViewManager.ankoView() instead")
-fun <T : View> __dslAddView(view: (ctx: Context) -> T, init: T.() -> Unit, manager: ViewManager): T {
-    return manager.ankoView({ ctx -> view(ctx) }) { init() }
-}
-
-@Deprecated("Use Context.ankoView() instead")
-fun <T : View> __dslAddView(view: (ctx: Context) -> T, init: T.() -> Unit, ctx: Context): T {
-    return ctx.ankoView({ ctx -> view(ctx) }) { init() }
-}
-
-@Deprecated("Use Activity.ankoView() instead")
-fun <T : View> __dslAddView(view: (ctx: Context) -> T, init: T.() -> Unit, act: Activity): T {
-    return act.ankoView({ ctx -> view(ctx) }) { init() }
-}
-
-@Deprecated("Use Context.ankoView() instead")
-fun <T : View> __dslAddView(view: (ctx: Context) -> T, init: T.() -> Unit, fragment: Fragment): T {
-    return (fragment.activity as Context).ankoView({ ctx -> view(ctx) }) { init() }
 }
 
 private fun applyStyle(v: View, style: (View) -> Unit) {
@@ -77,6 +56,7 @@ fun Activity.UI(init: UiHelper.() -> Unit): UiHelper = UI(true, init)
 
 fun Fragment.UI(init: UiHelper.() -> Unit): UiHelper = activity.UI(false, init)
 
+@Deprecated("Use AnkoContext instead")
 class UiHelper(val ctx: Context, private val setContentView: Boolean = true) : ViewManager {
     private lateinit var view: View
 
