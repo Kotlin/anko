e5eda071290909eaf80b2ac48b48348f3a035c5b
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-12-01 20:34:33 +0300

    Reusable contexts for list adapters

--- dsl/static/src/common/AnkoContext.kt
+++ dsl/static/src/common/AnkoContext.kt
@@ -21,9 +21,40 @@ interface AnkoContext<T> : ViewManager {
     override fun removeView(view: View) {
         throw UnsupportedOperationException()
     }
+
+    companion object {
+        fun create(ctx: Context): AnkoContext<Context> = AnkoContextImpl(ctx, ctx, false)
+        fun createReusable(ctx: Context): AnkoContext<Context> = ReusableAnkoContext(ctx, ctx, false)
+        fun <T> create(ctx: Context, owner: T): AnkoContext<T> = AnkoContextImpl(ctx, owner, false)
+        fun <T> createReusable(ctx: Context, owner: T): AnkoContext<T> = ReusableAnkoContext(ctx, owner, false)
+        fun <T: ViewGroup> createDelegate(owner: T): AnkoContext<T> = DelegatingAnkoContext(owner)
+    }
 }
 
-class AnkoContextImpl<T>(
+internal class DelegatingAnkoContext<T: ViewGroup>(override val owner: T): AnkoContext<T> {
+    override val ctx = owner.context
+    override val view: View = owner
+
+    override fun addView(view: View?, params: ViewGroup.LayoutParams?) {
+        if (view == null) return
+
+        if (params == null) {
+            owner.addView(view)
+        } else {
+            owner.addView(view, params)
+        }
+    }
+}
+
+internal class ReusableAnkoContext<T>(
+        override val ctx: Context,
+        override val owner: T,
+        private val setContentView: Boolean
+) : AnkoContextImpl<T>(ctx, owner, setContentView) {
+    override fun alreadyHasView() {}
+}
+
+open class AnkoContextImpl<T>(
         override val ctx: Context,
         override val owner: T,
         private val setContentView: Boolean
@@ -33,20 +64,25 @@ class AnkoContextImpl<T>(
     override val view: View
         get() = myView ?: throw IllegalStateException("View was not set previously")
 
-    override fun addView(view: View, params: ViewGroup.LayoutParams?) {
+    override fun addView(view: View?, params: ViewGroup.LayoutParams?) {
+        if (view == null) return
+
         if (myView != null) {
-            throw IllegalStateException("View is already set: $myView")
+            alreadyHasView()
         }
 
         this.myView = view
 
         if (setContentView) {
-            when (ctx) {
-                is Activity -> ctx.setContentView(view)
+            val context = ctx
+            when (context) {
+                is Activity -> context.setContentView(view)
                 else -> throw IllegalStateException("Context is not an Activity, can't set content view")
             }
         }
     }
+
+    open protected fun alreadyHasView(): Unit = throw IllegalStateException("View is already set: $myView")
 }
 
 @AnkoInternals.NoBinding
@@ -63,11 +99,3 @@ interface AnkoComponent<T> {
 
 fun AnkoComponent<out Activity>.setContentView(activity: Activity) =
         createView(AnkoContextImpl(activity, activity, true))
-
-class MyActivity : Activity() {
-    fun a() {
-        this.UI {
-
-        }
-    }
-}
