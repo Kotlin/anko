1ebf1ef4adf7ae90d3b466e3e519b8299ef6d102
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2016-04-29 21:45:49 +0300

    Minor: Fix compilation (generated sql parsers are now nullable as well)

--- anko/library/generator/src/org/jetbrains/android/anko/render/SqlParserHelperRenderer.kt
+++ anko/library/generator/src/org/jetbrains/android/anko/render/SqlParserHelperRenderer.kt
@@ -34,7 +34,7 @@ class SqlParserHelperRenderer(config: AnkoConfiguration) : Renderer(config) {
             append(buffer {
                 line("fun <$types, R> rowParser(parser: ($types) -> R): RowParser<R> {")
                 line("return object : RowParser<R> {")
-                line("override fun parseRow(columns: Array<Any>): R {")
+                line("override fun parseRow(columns: Array<Any?>): R {")
                 line("if (columns.size != $i)")
                 val s = if (i == 1) "" else "s"
                 indent.line("throw SQLiteException(\"Invalid row: $i column$s required\")")
