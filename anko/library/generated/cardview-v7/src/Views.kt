7c64c4bd9951de333277db947e3815ce268a1d1e
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-11-10 22:47:21 +0300

    Generate layout for CardView
     Fixes #357

--- anko/library/generated/cardview-v7/src/Views.kt
+++ anko/library/generated/cardview-v7/src/Views.kt
@@ -12,37 +12,37 @@ import android.os.Build
 import android.widget.*
 
 @PublishedApi
-internal object `$$Anko$Factories$CardviewV7View` {
-    val CARD_VIEW = { ctx: Context -> android.support.v7.widget.CardView(ctx) }
+internal object `$$Anko$Factories$CardviewV7ViewGroup` {
+    val CARD_VIEW = { ctx: Context -> _CardView(ctx) }
 }
 
 inline fun ViewManager.cardView(): android.support.v7.widget.CardView = cardView() {}
-inline fun ViewManager.cardView(init: (@AnkoViewDslMarker android.support.v7.widget.CardView).() -> Unit): android.support.v7.widget.CardView {
-    return ankoView(`$$Anko$Factories$CardviewV7View`.CARD_VIEW, theme = 0) { init() }
+inline fun ViewManager.cardView(init: (@AnkoViewDslMarker _CardView).() -> Unit): android.support.v7.widget.CardView {
+    return ankoView(`$$Anko$Factories$CardviewV7ViewGroup`.CARD_VIEW, theme = 0) { init() }
 }
 
 inline fun ViewManager.themedCardView(theme: Int = 0): android.support.v7.widget.CardView = themedCardView(theme) {}
-inline fun ViewManager.themedCardView(theme: Int = 0, init: (@AnkoViewDslMarker android.support.v7.widget.CardView).() -> Unit): android.support.v7.widget.CardView {
-return ankoView(`$$Anko$Factories$CardviewV7View`.CARD_VIEW, theme) { init() }
+inline fun ViewManager.themedCardView(theme: Int = 0, init: (@AnkoViewDslMarker _CardView).() -> Unit): android.support.v7.widget.CardView {
+return ankoView(`$$Anko$Factories$CardviewV7ViewGroup`.CARD_VIEW, theme) { init() }
 }
 
 inline fun Context.cardView(): android.support.v7.widget.CardView = cardView() {}
-inline fun Context.cardView(init: (@AnkoViewDslMarker android.support.v7.widget.CardView).() -> Unit): android.support.v7.widget.CardView {
-    return ankoView(`$$Anko$Factories$CardviewV7View`.CARD_VIEW, theme = 0) { init() }
+inline fun Context.cardView(init: (@AnkoViewDslMarker _CardView).() -> Unit): android.support.v7.widget.CardView {
+    return ankoView(`$$Anko$Factories$CardviewV7ViewGroup`.CARD_VIEW, theme = 0) { init() }
 }
 
 inline fun Context.themedCardView(theme: Int = 0): android.support.v7.widget.CardView = themedCardView(theme) {}
-inline fun Context.themedCardView(theme: Int = 0, init: (@AnkoViewDslMarker android.support.v7.widget.CardView).() -> Unit): android.support.v7.widget.CardView {
-return ankoView(`$$Anko$Factories$CardviewV7View`.CARD_VIEW, theme) { init() }
+inline fun Context.themedCardView(theme: Int = 0, init: (@AnkoViewDslMarker _CardView).() -> Unit): android.support.v7.widget.CardView {
+return ankoView(`$$Anko$Factories$CardviewV7ViewGroup`.CARD_VIEW, theme) { init() }
 }
 
 inline fun Activity.cardView(): android.support.v7.widget.CardView = cardView() {}
-inline fun Activity.cardView(init: (@AnkoViewDslMarker android.support.v7.widget.CardView).() -> Unit): android.support.v7.widget.CardView {
-    return ankoView(`$$Anko$Factories$CardviewV7View`.CARD_VIEW, theme = 0) { init() }
+inline fun Activity.cardView(init: (@AnkoViewDslMarker _CardView).() -> Unit): android.support.v7.widget.CardView {
+    return ankoView(`$$Anko$Factories$CardviewV7ViewGroup`.CARD_VIEW, theme = 0) { init() }
 }
 
 inline fun Activity.themedCardView(theme: Int = 0): android.support.v7.widget.CardView = themedCardView(theme) {}
-inline fun Activity.themedCardView(theme: Int = 0, init: (@AnkoViewDslMarker android.support.v7.widget.CardView).() -> Unit): android.support.v7.widget.CardView {
-return ankoView(`$$Anko$Factories$CardviewV7View`.CARD_VIEW, theme) { init() }
+inline fun Activity.themedCardView(theme: Int = 0, init: (@AnkoViewDslMarker _CardView).() -> Unit): android.support.v7.widget.CardView {
+return ankoView(`$$Anko$Factories$CardviewV7ViewGroup`.CARD_VIEW, theme) { init() }
 }
 
