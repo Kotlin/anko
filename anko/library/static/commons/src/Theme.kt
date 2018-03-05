2e2f4cf38bd65d44f37a43bd7d06711436b56e37
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2018-03-05 19:29:47 +0300

    Refactor theme related attribute resolving helpers

--- anko/library/static/commons/src/Theme.kt
+++ anko/library/static/commons/src/Theme.kt
@@ -3,44 +3,64 @@ package org.jetbrains.anko
 
 import android.app.Fragment
 import android.content.Context
-import android.os.Build
+import android.content.res.Resources
 import android.support.annotation.AttrRes
 import android.support.annotation.ColorInt
+import android.support.annotation.Dimension
 import android.util.TypedValue
 import android.view.View
 
+fun Resources.Theme.attr(@AttrRes attribute: Int): TypedValue {
+    val typedValue = TypedValue()
+    if (!resolveAttribute(attribute, typedValue, true)) {
+        throw IllegalArgumentException("Failed to resolve attribute: $attribute")
+    }
 
-fun Context.attr(@AttrRes attribute: Int): TypedValue {
-    var typed = TypedValue()
-    ctx.theme.resolveAttribute(attribute, typed, true)
-    return typed
+    return typedValue
 }
 
-//returns px
+@ColorInt
+fun Resources.Theme.color(@AttrRes attribute: Int): Int {
+    val attr = attr(attribute)
+    if (attr.type < TypedValue.TYPE_FIRST_COLOR_INT || attr.type > TypedValue.TYPE_LAST_COLOR_INT) {
+        throw IllegalArgumentException("Attribute value type is not color: $attribute")
+    }
+
+    return attr.data
+}
+
+fun Context.attr(@AttrRes attribute: Int): TypedValue = theme.attr(attribute)
+
+@Dimension(unit = Dimension.PX)
 fun Context.dimenAttr(@AttrRes attribute: Int): Int =
         TypedValue.complexToDimensionPixelSize(attr(attribute).data, resources.displayMetrics)
 
-//returns color
 @ColorInt
-fun Context.colorAttr(@AttrRes attribute: Int): Int {
-    return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-        resources.getColor(attr(attribute).resourceId, ctx.theme)
-    } else {
-        resources.getColor(attr(attribute).resourceId)
-    }
-}
+fun Context.colorAttr(@AttrRes attribute: Int): Int = theme.color(attribute)
 
+
+@Dimension(unit = Dimension.PX)
 inline fun AnkoContext<*>.dimenAttr(@AttrRes attribute: Int): Int = ctx.dimenAttr(attribute)
+
 @ColorInt
 inline fun AnkoContext<*>.colorAttr(@AttrRes attribute: Int): Int = ctx.colorAttr(attribute)
-inline fun AnkoContext<*>.attribute(@AttrRes attribute: Int): TypedValue = ctx.attr(attribute)
 
+inline fun AnkoContext<*>.attr(@AttrRes attribute: Int): TypedValue = ctx.attr(attribute)
+
+
+@Dimension(unit = Dimension.PX)
 inline fun View.dimenAttr(@AttrRes attribute: Int): Int = context.dimenAttr(attribute)
+
 @ColorInt
 inline fun View.colorAttr(@AttrRes attribute: Int): Int = context.colorAttr(attribute)
+
 inline fun View.attr(@AttrRes attribute: Int): TypedValue = context.attr(attribute)
 
+
+@Dimension(unit = Dimension.PX)
 inline fun Fragment.dimenAttr(@AttrRes attribute: Int): Int = activity.dimenAttr(attribute)
+
 @ColorInt
 inline fun Fragment.colorAttr(@AttrRes attribute: Int): Int = activity.colorAttr(attribute)
+
 inline fun Fragment.attr(@AttrRes attribute: Int): TypedValue = activity.attr(attribute)
